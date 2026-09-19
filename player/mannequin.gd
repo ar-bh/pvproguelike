d1549ebb8cba78ef9634e87b1ec3c619762d7818
web <146587835+ar-bh@users.noreply.github.com>
2026-09-19 18:27:16 -0400

    switching mannequin gender is now do-able in the player node rather than its mannequin child
    also started tps movement

--- player/mannequin.gd
+++ player/mannequin.gd
@@ -1,17 +1,18 @@
 @tool
-class_name Mannequin extends Node3D
+class_name Mannequin
+extends Node3D
 
 @onready var _mannequin_mesh: MeshInstance3D = %MannequinMesh
 
-@onready var mannequins: Dictionary = {
-	"m": preload("res://assets/mannequin/m_mannequin/m_mannequin.mesh"),
-	"f": preload("res://assets/mannequin/f_mannequin/f_mannequin.mesh"),
-}
-
-## The gender of the mannequin model
-@export var gender: Mesh:
+var gender: Mesh = preload("res://player/mannequin/m_mannequin.mesh"):
 	set = set_mannequin_gender
+
 
 func set_mannequin_gender(new_gender: Mesh) -> void:
 	gender = new_gender
+	if is_node_ready():
+		_mannequin_mesh.mesh = gender
+
+
+func _ready() -> void:
 	_mannequin_mesh.mesh = gender
--- player/player.gd
+++ player/player.gd
@@ -1,1 +1,37 @@
+@tool
+class_name Player
 extends CharacterBody3D
+
+@export var gender: Mesh = preload("res://player/mannequin/m_mannequin.mesh"):
+	set(new_gender):
+		gender = new_gender
+		if is_node_ready():
+			_mannequin.gender = gender
+
+@export_group("Camera")
+@export_range(0.0, 1.0) var mouse_sensitivity := 0.25
+
+var _camera_input_direction := Vector2.ZERO
+
+@onready var _mannequin: Mannequin = $Mannequin
+@onready var _camera_pivot: Node3D = $CameraPivot
+
+
+func _ready() -> void:
+	_mannequin.gender = gender
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion := (
+		event is InputEventMouseMotion
+		and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion:
+		_camera_input_direction = event.screen_relative * mouse_sensitivity
+
+
+func _physics_process(delta: float) -> void:
+	_camera_pivot.rotation.x += _camera_input_direction.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI / 6.0, PI / 3.0)
+	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+	_camera_input_direction = Vector2.ZERO
