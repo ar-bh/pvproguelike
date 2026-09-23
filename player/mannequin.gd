34521e95b954f1aa9c0a29bd90433e97dfe49e9a
web <146587835+ar-bh@users.noreply.github.com>
2026-09-23 19:33:00 -0400

    messed up punching and looking system

--- player/mannequin.gd
+++ player/mannequin.gd
@@ -4,6 +4,16 @@ extends Node3D
 
 var _visual: Node3D
 var _anim: AnimationPlayer
+var _tree: AnimationTree
+var _locomo: AnimationNodeAnimation
+var _locomo_a: AnimationNodeAnimation
+var _locomo_b: AnimationNodeAnimation
+var _locomo_xfade: AnimationNodeTransition
+var _overlay: AnimationNodeAnimation
+var _aim: PunchAimModifier
+var _use_b := false
+var _upper_age := 0.0
+var _current := &""
 
 var gender: PackedScene:
 	set = set_mannequin_gender
@@ -18,6 +28,14 @@ func set_mannequin_gender(new_gender: PackedScene) -> void:
 	if _visual:
 		_visual.free()
 		_visual = null
+	_tree = null
+	_locomo = null
+	_locomo_a = null
+	_locomo_b = null
+	_locomo_xfade = null
+	_overlay = null
+	_aim = null
+	_current = &""
 	_visual = gender.instantiate()
 	add_child(_visual)
 	_anim = _visual.find_child("AnimationPlayer", true, false) as AnimationPlayer
@@ -27,6 +45,13 @@ func set_mannequin_gender(new_gender: PackedScene) -> void:
 	if _anim.has_animation_library(&""):
 		_anim.remove_animation_library(&"")
 	_anim.add_animation_library(&"", ANIMS)
+	_anim.autoplay = ""
+	if Engine.is_editor_hint():
+		_anim.active = false
+		_anim.stop()
+		return
+	_configure_clip_modes()
+	_setup_upper()
 
 
 func _ready() -> void:
@@ -34,27 +59,150 @@ func _ready() -> void:
 		set_mannequin_gender(gender)
 
 
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	if is_punching():
+		_upper_age += delta
+	if _aim:
+		_aim.influence = move_toward(_aim.influence, 1.0 if _wants_look() else 0.0, delta / 0.25)
+
+
+func _configure_clip_modes() -> void:
+	for name in ANIMS.get_animation_list():
+		var anim := ANIMS.get_animation(name)
+		if anim == null:
+			continue
+		var n := String(name)
+		var oneshot := (
+			n == "Jump"
+			or "Start" in n or "Land" in n or "Enter" in n or "Exit" in n
+			or "Punch" in n or "Hit" in n
+		)
+		anim.loop_mode = Animation.LOOP_NONE if oneshot else Animation.LOOP_LINEAR
+
+
+func _setup_upper() -> void:
+	var skel := _visual.find_child("Skeleton3D", true, false) as Skeleton3D
+	if skel == null:
+		return
+	_tree = AnimationTree.new()
+	_visual.add_child(_tree)
+	_tree.anim_player = _tree.get_path_to(_anim)
+	_tree.callback_mode_process = AnimationMixer.ANIMATION_CALLBACK_MODE_PROCESS_PHYSICS
+
+	_locomo_a = AnimationNodeAnimation.new()
+	_locomo_a.animation = &"Idle"
+	_locomo_b = AnimationNodeAnimation.new()
+	_locomo_b.animation = &"Idle"
+	_overlay = AnimationNodeAnimation.new()
+	_overlay.animation = &"Punch_Jab"
+	_locomo_xfade = AnimationNodeTransition.new()
+	_locomo_xfade.xfade_time = 0.12
+	_locomo_xfade.add_input("a")
+	_locomo_xfade.add_input("b")
+	var scale_a := AnimationNodeTimeScale.new()
+	var scale_b := AnimationNodeTimeScale.new()
+
+	var shot := AnimationNodeOneShot.new()
+	shot.fadein_time = 0.25
+	shot.fadeout_time = 0.3
+	shot.mix_mode = AnimationNodeOneShot.MIX_MODE_BLEND
+	shot.filter_enabled = true
+	var prefix := _anim_track_prefix()
+	for b in skel.get_bone_count():
+		var bone := skel.get_bone_name(b)
+		var n := bone.to_lower()
+		if n == "root" or "pelvis" in n or "hip" in n or "thigh" in n or "calf" in n or "foot" in n or "ball" in n or "toe" in n:
+			continue
+		if (
+			"spine" in n or "chest" in n or "neck" in n or "head" in n
+			or "shoulder" in n or "clavicle" in n or "arm" in n or "hand" in n
+			or "finger" in n or "thumb" in n or "index" in n or "middle" in n
+			or "ring" in n or "pinky" in n
+		):
+			shot.set_filter_path(NodePath(prefix + bone), true)
+
+	var blend := AnimationNodeBlendTree.new()
+	blend.add_node("locomo_a", _locomo_a, Vector2(0, 0))
+	blend.add_node("locomo_b", _locomo_b, Vector2(0, 80))
+	blend.add_node("scale_a", scale_a, Vector2(90, 0))
+	blend.add_node("scale_b", scale_b, Vector2(90, 80))
+	blend.add_node("locomo_xfade", _locomo_xfade, Vector2(180, 40))
+	blend.add_node("overlay", _overlay, Vector2(0, 180))
+	blend.add_node("shot", shot, Vector2(380, 80))
+	blend.connect_node("scale_a", 0, "locomo_a")
+	blend.connect_node("scale_b", 0, "locomo_b")
+	blend.connect_node("locomo_xfade", 0, "scale_a")
+	blend.connect_node("locomo_xfade", 1, "scale_b")
+	blend.connect_node("shot", 0, "locomo_xfade")
+	blend.connect_node("shot", 1, "overlay")
+	blend.connect_node("output", 0, "shot")
+	_tree.tree_root = blend
+	_tree.active = true
+	_locomo = _locomo_a
+	_use_b = false
+	_current = &"Idle"
+	_aim = PunchAimModifier.new()
+	_aim.influence = 0.0
+	skel.add_child(_aim)
+	_aim.cache_bones(skel)
+
+
+func _anim_track_prefix() -> String:
+	if ANIMS.has_animation(&"Punch_Jab"):
+		var jab := ANIMS.get_animation(&"Punch_Jab")
+		if jab.get_track_count() > 0:
+			var p := String(jab.track_get_path(0))
+			var i := p.rfind(":")
+			if i >= 0:
+				return p.substr(0, i + 1)
+	return "Armature/Skeleton3D:"
+
+
 func _play(anim_name: StringName, sync_phase: bool = true, speed: float = 1.0) -> void:
-	if _anim == null or _anim.current_animation == anim_name:
+	if Engine.is_editor_hint():
 		return
-	var t := _anim.current_animation_position
-	var old_len := _anim.current_animation_length
-	var blend := 0.15
-	match _anim.current_animation:
-		&"Jump", &"Jump_Start", &"Jump_Land":
-			match anim_name:
-				&"Jump", &"Jump_Start", &"Jump_Land":
-					pass
-				_:
-					blend = 0.25
-					sync_phase = false
-	_anim.play(anim_name, blend, speed)
-	if not sync_phase or old_len <= 0.001:
+	if anim_name == _current:
 		return
-	var new_len := _anim.current_animation_length
-	if new_len <= 0.001:
+	if _locomo_xfade == null or _tree == null:
+		if _anim and _anim.current_animation != anim_name:
+			_anim.play(anim_name, 0.12, speed)
+			_current = anim_name
 		return
-	_anim.seek(fposmod(t / old_len * new_len, new_len), true)
+	_locomo_xfade.xfade_time = _xfade_time(_current, anim_name)
+	_use_b = not _use_b
+	if _use_b:
+		_locomo_b.animation = anim_name
+		_locomo = _locomo_b
+		_tree.set("parameters/scale_b/scale", speed)
+		_tree.set("parameters/locomo_xfade/transition_request", "b")
+	else:
+		_locomo_a.animation = anim_name
+		_locomo = _locomo_a
+		_tree.set("parameters/scale_a/scale", speed)
+		_tree.set("parameters/locomo_xfade/transition_request", "a")
+	_current = anim_name
+
+
+func _xfade_time(from: StringName, to: StringName) -> float:
+	if to == &"Jump_Start" or to == &"Jump" or to == &"Jump_Loop":
+		return 0.08
+	if to == &"Jump_Land":
+		return 0.12
+	if _is_jump_clip(from):
+		return 0.2
+	if to == &"Idle" or to == &"Crouch_Idle":
+		return 0.2
+	return 0.12
+
+
+func _is_jump_clip(anim_name: StringName) -> bool:
+	match anim_name:
+		&"Jump", &"Jump_Start", &"Jump_Loop", &"Jump_Land":
+			return true
+		_:
+			return false
 
 
 func idle() -> void:
@@ -162,25 +310,123 @@ func jump_land() -> void:
 
 
 func slide_start() -> void:
+	_stop_upper()
 	_play(&"Slide_Start", false)
 
 
 func slide() -> void:
+	_stop_upper()
 	_play(&"Slide", false)
 
 
 func slide_exit() -> void:
+	_stop_upper()
 	_play(&"Slide_Exit", false)
 
 
+#region fists
+func set_punch_look(world_dir: Vector3) -> void:
+	if _aim == null or world_dir.length_squared() < 0.0001:
+		return
+	var local := global_transform.basis.inverse() * world_dir.normalized()
+	_aim.target_pitch = atan2(-local.y, local.z)
+
+
+func punch_jab() -> void:
+	_play_upper(&"Punch_Jab")
+
+
+func punch_cross() -> void:
+	_play_upper(&"Punch_Cross")
+
+
+func knockback() -> void:
+	_stop_upper()
+	_play(&"Hit_From_Front", false)
+
+
+func _play_upper(anim_name: StringName) -> void:
+	if _tree == null or _overlay == null:
+		_play(anim_name, false)
+		return
+	_overlay.animation = anim_name
+	_tree.set("parameters/shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	_upper_age = 0.0
+
+
+func _stop_upper() -> void:
+	if _tree:
+		_tree.set("parameters/shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
+	_upper_age = 0.0
+#endregion
+
+
 func is_transition() -> bool:
-	if _anim == null or not _anim.is_playing():
-		return false
-	match _anim.current_animation:
-		&"Crouch_Enter", &"Crouch_Exit", &"Slide_Start", &"Slide_Exit":
+	match _current:
+		&"Crouch_Enter", &"Crouch_Exit", &"Slide_Start", &"Slide_Exit", &"Hit_From_Front":
 			return true
 		_:
 			return false
 
+
 func is_slide_start() -> bool:
-	return _anim != null and _anim.is_playing() and _anim.current_animation == &"Slide_Start"
+	return _current == &"Slide_Start"
+
+
+func is_punching() -> bool:
+	return _tree != null and bool(_tree.get("parameters/shot/active"))
+
+
+func can_chain_punch() -> bool:
+	return is_punching() and _upper_age >= 0.2
+
+
+func is_knockback() -> bool:
+	return _current == &"Hit_From_Front"
+
+
+func _wants_look() -> bool:
+	if is_punching():
+		return true
+	match _current:
+		&"Idle", &"Crouch_Idle":
+			return true
+		_:
+			return false
+
+
+class PunchAimModifier extends SkeletonModifier3D:
+	var pitch := 0.0
+	var target_pitch := 0.0
+	var _spine: PackedInt32Array
+	var _weights: PackedFloat32Array
+
+	func cache_bones(skel: Skeleton3D) -> void:
+		var idxs: Array[int] = []
+		for i in skel.get_bone_count():
+			if skel.get_bone_name(i).to_lower().begins_with("spine"):
+				idxs.append(i)
+		_spine = PackedInt32Array(idxs)
+		var n := _spine.size()
+		_weights.resize(n)
+		var total := 0.0
+		for i in n:
+			var w := float(n - i)
+			_weights[i] = w
+			total += w
+		if total > 0.0:
+			for i in n:
+				_weights[i] /= total
+
+	func _process_modification_with_delta(delta: float) -> void:
+		var skel := get_skeleton()
+		if skel == null or _spine.is_empty():
+			return
+		if delta > 0.0:
+			pitch = lerpf(pitch, target_pitch, 1.0 - exp(-14.0 * delta))
+		if is_zero_approx(pitch):
+			return
+		for i in _spine.size():
+			var idx := _spine[i]
+			var extra := Quaternion(Vector3.RIGHT, pitch * _weights[i])
+			skel.set_bone_pose_rotation(idx, skel.get_bone_pose_rotation(idx) * extra)
--- player/player.gd
+++ player/player.gd
@@ -25,7 +25,6 @@ enum Gender { MALE, FEMALE }
 @export var model_sprint_turn_speed := 15.0
 
 var rotation_lock := true
-
 var _camera_input_direction := Vector2.ZERO
 #endregion
 
@@ -42,47 +41,64 @@ var _camera_input_direction := Vector2.ZERO
 @export var slide_speed_curve: Curve
 #endregion
 
-#region node variables
+#region fists
+var _punch_queued := false
+#endregion
+
+#region nodes
 @onready var _mannequin: Mannequin = $Mannequin
 @onready var _camera_pivot: Node3D = $CameraPivot
 @onready var _spring_arm: SpringArm3D = $CameraPivot/SpringArm3D
 @onready var _camera: Camera3D = %Camera3D
 #endregion
 
+#region state
+var movement_enabled := true
+
+var _was_crouching := false
+var _sliding := false
+var _slide_time := 0.0
+var _slide_dir := Vector3.ZERO
+var _slide_jump_queued := false
+var _jump_start_lock := 0.0
+var _in_jump := false
+var _land_lock := 0.0
+var _was_airborne := false
+var _walk_octant := 0
+var _crouch_octant := 0
+var _floor_snap := 0.35
+#endregion
+
+#region lifecycle
 func _ready() -> void:
 	_apply_gender()
+	_floor_snap = floor_snap_length
 	_spring_arm.add_excluded_object(get_rid())
 	if not Engine.is_editor_hint():
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 
 func _apply_gender() -> void:
 	if _mannequin:
 		_mannequin.gender = genders[gender]
 
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Engine.is_editor_hint():
 		return
-
 	if event is InputEventMouseButton:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
 	var is_camera_motion := (
 		event is InputEventMouseMotion
 		and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED
 	)
 	if is_camera_motion:
 		_camera_input_direction = event.screen_relative * mouse_sensitivity
+#endregion
 
-var _was_crouching := false
-
-var _sliding := false
-var _slide_time := 0.0
-var _slide_dir := Vector3.ZERO
-var _slide_jump_queued := false
-var _jump_start_lock := 0.0
-
+#region physics
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
@@ -90,7 +106,6 @@ func _physics_process(delta: float) -> void:
 	_camera_pivot.rotation.x += _camera_input_direction.y * delta
 	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
 	_camera_input_direction = Vector2.ZERO
-
 	var look := Input.get_vector("look_left", "look_right", "look_up", "look_down")
 	_camera_pivot.rotation.y -= look.x * gamepad_sensitivity * delta
 	_camera_pivot.rotation.x += look.y * gamepad_sensitivity * delta
@@ -99,12 +114,6 @@ func _physics_process(delta: float) -> void:
 	_apply_gravity(delta)
 	var input := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := _get_move_direction(input)
-	
-	if rotation_lock:
-		_face_camera(delta)
-	else:
-		_face_vector(delta, direction)
-
 	var crouching := Input.is_action_pressed("crouch")
 	var sprinting := (
 		Input.is_action_pressed("sprint")
@@ -113,7 +122,8 @@ func _physics_process(delta: float) -> void:
 	)
 
 	if (
-		not _sliding
+		movement_enabled
+		and not _sliding
 		and is_on_floor()
 		and Input.is_action_just_pressed("crouch")
 		and Input.is_action_pressed("sprint")
@@ -126,23 +136,40 @@ func _physics_process(delta: float) -> void:
 		if _slide_time >= slide_duration:
 			_stop_slide()
 
-	if _sliding:
+	var punching := _mannequin.is_punching()
+	if Input.is_action_just_pressed("attack") and not _sliding:
+		if punching and _mannequin.can_chain_punch():
+			_punch_queued = true
+		elif not punching:
+			_mannequin.punch_jab()
+			_punch_queued = false
+			punching = true
+
+	movement_enabled = not _mannequin.is_knockback()
+
+	if punching:
+		_face_camera(delta)
+	elif _sliding:
 		rotation_lock = false
 		_face_vector(delta, _slide_dir)
 	elif rotation_lock:
 		_face_camera(delta)
 	else:
 		_face_vector(delta, direction)
-
 	rotation_lock = not sprinting and not _sliding
+	_mannequin.set_punch_look(-_camera.global_basis.z)
 
-	var jumping := is_on_floor() and (
+	var jumping := is_on_floor() and movement_enabled and (
 		Input.is_action_just_pressed("jump")
 		or (_slide_jump_queued and _sliding and _slide_time >= slide_jump_lock)
 	)
-
 	var speed := move_speed
-	if _sliding:
+	if punching:
+		speed = crouch_speed
+	if not movement_enabled:
+		velocity.x = 0.0
+		velocity.z = 0.0
+	elif _sliding:
 		var t := clampf(_slide_time / slide_duration, 0.0, 1.0)
 		var falloff := 1.0
 		if slide_speed_curve:
@@ -151,7 +178,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = _slide_dir.x * speed
 		velocity.z = _slide_dir.z * speed
 	else:
-		if crouching:
+		if punching:
+			speed = crouch_speed
+		elif crouching:
 			speed = crouch_speed
 		elif sprinting:
 			speed = sprint_speed
@@ -170,38 +199,66 @@ func _physics_process(delta: float) -> void:
 			velocity.x = dir.x * keep
 			velocity.z = dir.z * keep
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and movement_enabled:
 		if _sliding and _slide_time < slide_jump_lock:
 			_slide_jump_queued = true
 		else:
 			velocity.y = jump_velocity
-			_jump_start_lock = 0.12
+			_jump_start_lock = 0.25
+			_in_jump = true
 			_mannequin.jump_start()
 			if _sliding:
 				_stop_slide(false)
 	elif _slide_jump_queued and _sliding and is_on_floor() and _slide_time >= slide_jump_lock:
 		velocity.y = jump_velocity
-		_jump_start_lock = 0.12
+		_jump_start_lock = 0.25
+		_in_jump = true
 		_mannequin.jump_start()
 		_stop_slide(false)
 
-	var was_on_floor := is_on_floor()
+	if _jump_start_lock > 0.0 or velocity.y > 0.05:
+		floor_snap_length = 0.0
+	else:
+		floor_snap_length = _floor_snap
+
+	if _land_lock > 0.0:
+		_land_lock = maxf(_land_lock - delta, 0.0)
+
 	move_and_slide()
-
+	
 	var on_floor := is_on_floor()
-	var just_landed := not was_on_floor and on_floor
-
+	if on_floor and velocity.y <= 0.0 and _jump_start_lock <= 0.0:
+		_in_jump = false
+	var airborne := _in_jump or not on_floor or _jump_start_lock > 0.0
+	var just_landed := _was_airborne and not airborne
+	_was_airborne = airborne
 	if _jump_start_lock > 0.0:
 		_jump_start_lock = maxf(_jump_start_lock - delta, 0.0)
 
-	if _sliding:
-		if _slide_time >= slide_jump_lock and not _mannequin.is_transition():
-			_mannequin.slide()
-	elif not on_floor:
+	if _mannequin.is_knockback():
+		pass
+	elif _mannequin.is_punching():
+		if _punch_queued and _mannequin.can_chain_punch():
+			_mannequin.punch_cross()
+			_punch_queued = false
+		if airborne:
+			if _jump_start_lock <= 0.0:
+				_mannequin.jump()
+		elif input == Vector2.ZERO:
+			_mannequin.idle()
+		else:
+			_play_walk(input)
+	elif airborne:
 		if _jump_start_lock <= 0.0:
 			_mannequin.jump()
+	elif _sliding:
+		if _slide_time >= slide_jump_lock and not _mannequin.is_transition():
+			_mannequin.slide()
 	elif just_landed and input == Vector2.ZERO:
 		_mannequin.jump_land()
+		_land_lock = 0.22
+	elif _land_lock > 0.0 and input == Vector2.ZERO:
+		pass
 	elif crouching and not _was_crouching:
 		_mannequin.crouch_enter()
 	elif not crouching and _was_crouching:
@@ -222,8 +279,9 @@ func _physics_process(delta: float) -> void:
 
 	if not _sliding:
 		_was_crouching = crouching
-		
+#endregion
 
+#region move
 func _get_move_direction(input: Vector2) -> Vector3:
 	var direction := (_camera.global_basis.x * input.x) + (_camera.global_basis.z * input.y)
 	direction.y = 0.0
@@ -231,21 +289,6 @@ func _get_move_direction(input: Vector2) -> Vector3:
 		return Vector3.ZERO
 	return direction
 
-func _start_slide(direction: Vector3) -> void:
-	_sliding = true
-	_slide_time = 0.0
-	_slide_dir = direction.normalized()
-	_was_crouching = true
-	_mannequin.slide_start()
-
-func _stop_slide(play_exit: bool = true) -> void:
-	if not _sliding:
-		return
-	_sliding = false
-	_slide_jump_queued = false
-	if play_exit:
-		_mannequin.slide_exit()
-	_was_crouching = Input.is_action_pressed("crouch")
 
 func _apply_gravity(delta: float) -> void:
 	if is_on_floor():
@@ -255,6 +298,26 @@ func _apply_gravity(delta: float) -> void:
 		gravity *= fall_gravity_multiplier
 	velocity += gravity * delta
 
+
+func _start_slide(direction: Vector3) -> void:
+	_sliding = true
+	_slide_time = 0.0
+	_slide_dir = direction.normalized()
+	_was_crouching = true
+	_mannequin.slide_start()
+
+
+func _stop_slide(play_exit: bool = true) -> void:
+	if not _sliding:
+		return
+	_sliding = false
+	_slide_jump_queued = false
+	if play_exit:
+		_mannequin.slide_exit()
+	_was_crouching = Input.is_action_pressed("crouch")
+#endregion
+
+#region facing
 func _face_camera(delta: float) -> void:
 	var look := -_camera.global_basis.z
 	look.y = 0.0
@@ -268,6 +331,7 @@ func _face_camera(delta: float) -> void:
 		clampf(model_turn_speed * delta, 0.0, 1.0)
 	)
 
+
 func _face_vector(delta: float, look: Vector3) -> void:
 	look.y = 0.0
 	if look.length_squared() < 0.0001:
@@ -279,11 +343,26 @@ func _face_vector(delta: float, look: Vector3) -> void:
 		target_yaw,
 		clampf(model_sprint_turn_speed * delta, 0.0, 1.0)
 	)
+#endregion
+
+#region anims
+func _stick_octant(input: Vector2, current: int) -> int:
+	var stick := Vector2(input.x, -input.y)
+	if stick.length_squared() < 0.0001:
+		return current
+	var ang := atan2(stick.x, stick.y)
+	var raw := wrapi(int(round(ang / TAU * 8.0)), 0, 8)
+	if raw == current:
+		return current
+	var center := float(current) / 8.0 * TAU
+	if absf(angle_difference(ang, center)) < deg_to_rad(28.0):
+		return current
+	return raw
+
 
 func _play_walk(input: Vector2) -> void:
-	var stick := Vector2(input.x, -input.y)  # W = up
-	var octant := wrapi(int(round(atan2(stick.x, stick.y) / TAU * 8.0)), 0, 8)
-	match octant:
+	_walk_octant = _stick_octant(input, _walk_octant)
+	match _walk_octant:
 		0:
 			_mannequin.walk_fwd()
 		1:
@@ -300,11 +379,11 @@ func _play_walk(input: Vector2) -> void:
 			_mannequin.walk_l()
 		7:
 			_mannequin.walk_fwd_l()
-			
+
+
 func _play_crouch(input: Vector2) -> void:
-	var stick := Vector2(input.x, -input.y)
-	var octant := wrapi(int(round(atan2(stick.x, stick.y) / TAU * 8.0)), 0, 8)
-	match octant:
+	_crouch_octant = _stick_octant(input, _crouch_octant)
+	match _crouch_octant:
 		0:
 			_mannequin.crouch_fwd()
 		1:
@@ -321,3 +400,4 @@ func _play_crouch(input: Vector2) -> void:
 			_mannequin.crouch_left()
 		7:
 			_mannequin.crouch_fwd_l()
+#endregion
